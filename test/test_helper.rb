463144b5059ca35fb38c59a2be9d854574e8e7c0
Kyle Wiest <kyle.wiest@gmail.com>
2013-01-15 15:27:22 -0800

    Remove Factory

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,9 +8,3 @@ require 'rails/test_help'
 
 # Run the migrations
 ActiveRecord::Migrator.migrate("#{Rails.root}/db/migrate")
-
-# Mixin the factory helper
-require File.expand_path("#{File.dirname(__FILE__)}/factory")
-Test::Unit::TestCase.class_eval do
-  include Factory
-end
