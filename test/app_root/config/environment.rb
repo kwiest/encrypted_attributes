8186acd3ee1ce016e8c18276dcb61cb90b288c7b
Kyle Wiest <kyle.wiest@gmail.com>
2013-02-08 08:31:20 -0800

    Remove old rails app test dir

--- test/app_root/config/environment.rb
+++
@@ -1,9 +0,0 @@
-require 'config/boot'
-
-Rails::Initializer.run do |config|
-  config.plugin_paths << '..'
-  config.plugins = %w(encrypted_strings encrypted_attributes)
-  config.cache_classes = false
-  config.whiny_nils = true
-  config.action_controller.session = {:key => 'rails_session', :secret => 'd229e4d22437432705ab3985d4d246'}
-end
