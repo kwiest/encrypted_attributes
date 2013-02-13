8f889f076e9ee4115bd1d8f1045d179e90cf9125
Kyle Wiest <kyle.wiest@gmail.com>
2013-02-12 16:42:54 -0800

    Require Pry for help debugging

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,7 @@ require 'bundler/setup'
 $:.unshift File.dirname(__FILE__)
 require 'rails_app/config/environment'
 require 'rails/test_help'
+require 'pry'
 
 # Run the migrations
 ActiveRecord::Migrator.migrate("#{Rails.root}/db/migrate")
