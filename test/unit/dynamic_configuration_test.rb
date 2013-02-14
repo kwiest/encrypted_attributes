4c7e384f200fe66eb63067ecdd026450ff6459eb
Kyle Wiest <kyle.wiest@gmail.com>
2013-02-13 17:06:46 -0800

    Add dynamic config test

---
+++ test/unit/dynamic_configuration_test.rb
@@ -0,0 +1,32 @@
+$:.unshift File.dirname(__FILE__) + '/..'
+require 'test_helper'
+
+class DynamicConfigurationTest < ActiveSupport::TestCase
+  def setup
+    @salt = nil
+
+    User.encrypts :password, :before => lambda { |record| record.salt = record.login } do |user|
+      { :salt => @salt = user.salt }
+    end
+    
+    @user = User.create! :login => 'admin', :password => 'secret'
+  end
+  
+  def test_should_use_dynamic_configuration_during_write
+    assert_equal 'a55d037f385cad22efe7862e07b805938d150154', @user.password
+  end
+  
+  def test_should_use_dynamic_configuration_during_read
+    user = User.find @user.id
+    assert_equal 'a55d037f385cad22efe7862e07b805938d150154', @user.password
+  end
+  
+  def test_should_build_configuration_after_before_callbacks_invoked
+    assert_equal 'admin', @salt
+  end
+
+  def teardown
+    User.reset_callbacks :encrypt_password
+    super
+  end
+end
